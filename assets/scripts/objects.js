c0b9beaae8d32c440b003390b7aa33447cb922ce
kimba <skyqwn@gmail.com>
2022-09-08 09:27:38 +0900

    javascript 연습중

---
+++ assets/scripts/objects.js
@@ -0,0 +1,92 @@
+const addMovieBtn = document.getElementById("add-movie-btn");
+const searchBtn = document.getElementById("search-btn");
+
+const movies = [];
+
+const renderMovies = (filter = "") => {
+  //필터가 Falsy일때 모든 영화가 표시된다.
+  const movieList = document.getElementById("movie-list");
+
+  if (movies.lenth === 0) {
+    movieList.classList.remove("visible");
+    return;
+  } else {
+    movieList.classList.add("visible");
+  }
+  movieList.innerHTML = "";
+
+  const filteredMovies = !filter
+    ? movies
+    : movies.filter((movie) => movie.info.title.includes(filter));
+
+  filteredMovies.forEach((movie) => {
+    const movieEl = document.createElement("li");
+    // if (!("info" in movie)) {}
+    // if(movie.info === undefined){}
+    const { info, ...otherProps } = movie;
+    console.log(otherProps);
+    // const { title: movieTitle } = info;
+    let { getFormattedTitle } = movie;
+    // getFormattedTitle = getFormattedTitle.bind(movie);
+    let text = getFormattedTitle.call(movie) + " - ";
+    for (const key in info) {
+      if (key !== "title" && key !== "_title") {
+        text = text + `${key}: ${info[key]}`;
+      }
+    }
+    movieEl.textContent = text;
+    movieList.append(movieEl);
+  });
+};
+
+const addMoiveHandler = () => {
+  const title = document.getElementById("title").value;
+  const extraName = document.getElementById("extra-name").value;
+  const extraValue = document.getElementById("extra-value").value;
+
+  if (
+    title.trim() === "" ||
+    extraName.trim() === "" ||
+    extraValue.trim() === ""
+  ) {
+    return;
+  }
+  const newMovie = {
+    info: {
+      set title(val) {
+        if (val.trim() === "") {
+          this.title = "DEFAULT";
+          return;
+        }
+        this._title = val;
+      },
+      get title() {
+        return this._title;
+      },
+      [extraName]: extraValue,
+    },
+    id: Math.random().toString(),
+    // getFormattedTitle: function () {
+    //   return this.info.title.toUpperCase();
+    // }, 아래와 같이 객체내 매서드를 축약해서 쓸수 있다.
+    getFormattedTitle() {
+      console.log(this);
+      return this.info.title.toUpperCase();
+    },
+  };
+
+  newMovie.info.title = title;
+  console.log(newMovie.info.title);
+
+  movies.push(newMovie);
+  renderMovies();
+};
+
+const searchMoiveHandler = () => {
+  console.log(this);
+  const filterTerm = document.getElementById("filter-title").value;
+  renderMovies(filterTerm);
+};
+
+addMovieBtn.addEventListener("click", addMoiveHandler);
+searchBtn.addEventListener("click", searchMoiveHandler);
